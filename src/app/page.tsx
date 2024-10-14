4a711054d66a204780801295cd983737411d0ffb
VikashPR <vikashrajendran2806@gmail.com>
2024-10-14 09:41:11 +0000

    Remove navigation links from home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="/performance">Performance</Link>
-      <Link href="/reliability">Reliability</Link>
-      <Link href="/scale">Scale</Link>
       Home Page
     </div>
   );
