5cecd5f4c23724abae76e66b66dabe464a814866
VikashPR <vikashrajendran2806@gmail.com>
2024-10-14 09:13:52 +0000

    Add empty performance page component

---
+++ src/app/performance/page.tsx
@@ -0,0 +1,10 @@
+const PerformancePage = () => {
+    return (
+        <div>
+        <h1>Performance</h1>
+        <p>Performance page content</p>
+        </div>
+    );
+}
+    
+export default PerformancePage;
