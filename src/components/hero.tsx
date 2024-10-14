27ae116e8b39cf0fb92f1796a3e0b49418fcbdce
VikashPR <vikashrajendran2806@gmail.com>
2024-10-14 11:17:59 +0000

    Refactor Hero component to add background image and title props

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -9,6 +9,19 @@ interface HeroProps {
 
 const Hero = (props: HeroProps) => {
     return (
-        
-    )   
+      <div className="relative h-screen">
+        <div className="absolute -z-10 inset-0">
+          <Image
+            src={props.imgData}
+            alt={props.imgAlt}
+            fill
+            style={{ objectFit: "cover" }}
+          />
+            </div>
+            
+            <div className="pt-48 flex justify-center items-center">
+                <h1 className="text-white text-6xl">{props.title}</h1>
+            </div>
+      </div>
+    );   
 }
