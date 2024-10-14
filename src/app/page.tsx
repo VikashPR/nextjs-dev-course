e3457249264be663b85f4d88f59bb8dcd11f4795
VikashPR <vikashrajendran2806@gmail.com>
2024-10-14 11:27:41 +0000

    Refactor page component to use Hero component with image and title props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import homeImg from '../../public/home.jpg';
+import Hero from "@/components/hero";
+import homeImg from "../../public/home.jpg";
 
 export default function Home() {
   return (
     <div>
-      Home Page
-      <div className="absolute -z-10 inset-0">
-        <Image src={homeImg} alt="car factory" fill style={{objectFit: 'cover'}}/>
-      </div>
+      <Hero
+        imgData={homeImg}
+        imgAlt="Hero image"
+        title="Professional Cloud Hosting"
+      />
     </div>
   );
 }
