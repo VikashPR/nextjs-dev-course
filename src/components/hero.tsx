7d77e8b8f5cfc502c924703b4865cb7b9a0b0f61
VikashPR <vikashrajendran2806@gmail.com>
2024-10-14 11:15:33 +0000

    Add Hero component with image and title props

---
+++ src/components/hero.tsx
@@ -0,0 +1,14 @@
+import type { StaticImageData } from "next/image";
+import Image from "next/image";
+
+interface HeroProps {
+    imgData: StaticImageData;
+    imgAlt: string;
+    title: string;
+}
+
+const Hero = (props: HeroProps) => {
+    return (
+        
+    )   
+}
