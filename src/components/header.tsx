dbe999eb713289a72fdd0f931e83fb70b24b94c5
VikashPR <vikashrajendran2806@gmail.com>
2024-10-28 09:58:40 +0000

    Refactor header component to update navigation styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,13 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <div>
-        <Link href="/">Home</Link>
-        <Link href="/performance">Performance</Link>
-        <Link href="/reliability">Reliability</Link>
-        <Link href="/scale">Scale</Link>
+        <div className="w-full absolute text-white z-10">
+        <nav className="container relative flex flex-wrap items-center justify-between mx-auto p-8">
+            <Link href="/">Home</Link>
+            <Link href="/performance">Performance</Link>
+            <Link href="/reliability">Reliability</Link>
+            <Link href="/scale">Scale</Link>
+        </nav>
         </div>
     );
 }
